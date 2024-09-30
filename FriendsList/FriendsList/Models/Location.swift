393a82b957168445a07fbd39805ddd8c8a24bb48
ealpago <alpagoemre@icloud.com>
2024-09-30 19:42:01 +0300

    Location's postcode parameter bug fixed

--- FriendsList/FriendsList/Models/Location.swift
+++ FriendsList/FriendsList/Models/Location.swift
@@ -12,7 +12,7 @@ struct Location: Codable {
     let city: String?
     let coordinates: Coordinate?
     let country: String?
-    let postcode: Int?
+    let postcode: Postcode?
     let state: String?
     let street: Street?
     let timezone: Timezone?
@@ -25,5 +25,31 @@ struct Location: Codable {
         case state = "state"
         case street
         case timezone
+    }
+}
+
+enum Postcode: Codable {
+    case int(Int)
+    case string(String)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let intValue = try? container.decode(Int.self) {
+            self = .int(intValue)
+        } else if let stringValue = try? container.decode(String.self) {
+            self = .string(stringValue)
+        } else {
+            throw DecodingError.typeMismatch(Postcode.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Expected to decode Int or String for postcode"))
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .int(let value):
+            try container.encode(value)
+        case .string(let value):
+            try container.encode(value)
+        }
     }
 }
