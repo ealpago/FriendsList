8cbb669eb619025b837259ba8df9f3b3b9921ae6
ealpago <alpagoemre@icloud.com>
2024-09-30 23:15:38 +0300

    DetailViewMdoel marked

--- FriendsList/FriendsList/Modules/Detail/DetailViewModel.swift
+++ FriendsList/FriendsList/Modules/Detail/DetailViewModel.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
+//MARK: Protocol
 protocol DetailViewModelInterface {
     func viewDidLoad()
 }
 
+//MARK: ViewModel
 final class DetailViewModel {
+
+    //MARK: Properties
     weak var view: DetailViewInterface?
 }
 
+//MARK: Extension
 extension DetailViewModel: DetailViewModelInterface {
     func viewDidLoad() {
         view?.prepareUIConstraints()
