346c674cf463d6e48484d22512745e97f5936194
ealpago <alpagoemre@icloud.com>
2024-09-30 19:11:42 +0300

    ListingViewModel ready for business

--- FriendsList/FriendsList/Modules/Listing/ListingViewModel.swift
+++ FriendsList/FriendsList/Modules/Listing/ListingViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+protocol ListingViewModelInterface {
+    var numberOfRowsInSection: Int { get }
+
+    func viewDidLoad()
+    func cellForRow(at index: Int)
+}
+
+final class ListingViewModel {
+    weak var view: ListingViewInterface?
+    private var users = [ResponseResult]()
+
+    func fetchData() {
+        view?.showLoadingIndicator()
+        RandomUserManager.shared.fetchRandomUser { [weak self] result in
+            self?.view?.hideLoadingIndicator()
+            switch result {
+            case .success(let response):
+                guard let userList = response.results else { return }
+                self?.users = userList
+            case .failure(let error):
+                self?.view?.showError(title: "Error", message: error.localizedDescription, buttonTitle: "OK", completion: {
+                    self?.view?.popToRoot()
+                })
+            }
+        }
+    }
+}
+
+extension ListingViewModel: ListingViewModelInterface{
+    func viewDidLoad() {
+        view?.prepareTableView()
+        fetchData()
+        view?.reloadData()
+    }
+    
+    var numberOfRowsInSection: Int {
+        users.count
+    }
+    
+    func cellForRow(at index: Int) {}
+}
