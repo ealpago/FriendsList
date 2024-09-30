56c51bb55130be31b416b6d811b478c9acc4bec6
ealpago <alpagoemre@icloud.com>
2024-10-01 02:35:01 +0300

    RealmManager fixed fo migrate bug

--- FriendsList/FriendsList/Managers/RealmManager.swift
+++ FriendsList/FriendsList/Managers/RealmManager.swift
@@ -10,13 +10,35 @@ import RealmSwift
 class RealmManager {
     static let shared = RealmManager()
 
-    func saveUserToRealm(user: CachedUser) {
+    init() {
+        performMigration()
+    }
+
+    private func performMigration() {
+        let config = Realm.Configuration(
+            schemaVersion: 1, // Increment this number whenever you change the schema
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < 1 {
+                    // Migration logic, if needed
+                    migration.enumerateObjects(ofType: CachedUser.className()) { oldObject, newObject in
+                        // Initialize the new 'id' property with default values or migrate existing data
+                        newObject!["id"] = UUID().uuidString
+                    }
+                }
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
+    }
+
+    func saveUserToRealm(user: ResponseResult) {
         do {
             let realm = try Realm()
             let cachedUser = CachedUser()
-            cachedUser.name = user.name
-            cachedUser.surname = user.surname  // Changed to surname from name
-            cachedUser.nationality = user.nationality
+            cachedUser.id = user.id?.value ?? UUID().uuidString
+            cachedUser.name = user.name?.first ?? ""
+            cachedUser.surname = user.name?.last ?? ""
+            cachedUser.nationality = user.nat ?? ""
+            cachedUser.imageURL = user.picture?.thumbnail ?? ""
             try realm.write {
                 realm.add(cachedUser, update: .modified)
             }
@@ -24,5 +46,16 @@ class RealmManager {
             print("Error while saving realm object: \(error)")
         }
     }
-}
+
+    func fetchUsersFromRealm() -> [CachedUser] {
+        do {
+            let realm = try Realm()
+            let users = realm.objects(CachedUser.self)
+            print("Fetched Users: \(users)")
+            return Array(users)
+        } catch {
+            print("Error fetching users from Realm: \(error)")
+            return []
+        }
+    }}
 
