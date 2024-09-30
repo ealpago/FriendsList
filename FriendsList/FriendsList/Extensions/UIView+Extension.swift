339b6b57ba69d32795e926412c692d92b9abc880
ealpago <alpagoemre@icloud.com>
2024-09-30 16:41:48 +0300

    UIView+Extension added

---
+++ FriendsList/FriendsList/Extensions/UIView+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIView+Extension.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import UIKit
+
+extension UIView {
+    public static var bundle: Bundle {
+        get {
+            Bundle(for: self)
+        }
+    }
+
+    public static var identifier: String {
+        String(describing: self)
+    }
+
+    public static var nib: UINib {
+        get {
+            UINib(nibName: identifier, bundle: bundle)
+        }
+    }
+}
