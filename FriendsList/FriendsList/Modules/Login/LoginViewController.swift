e91c73beb6e941edc719f1c3ff9e06f2b23d1123
ealpago <alpagoemre@icloud.com>
2024-09-30 17:05:49 +0300

    LoginVC base mvvm ready

---
+++ FriendsList/FriendsList/Modules/Login/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import UIKit
+
+protocol LoginViewInterface: AnyObject, AlertPresentable {
+    var userName: String { get }
+    var password: String { get }
+
+    func pushVC()
+}
+
+final class LoginViewController: UIViewController {
+
+    @IBOutlet private weak var stackView: UIStackView!
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var userNameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+
+    //MARK: Properties
+    private lazy var viewModel = LoginViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.view = self
+        viewModel.viewDidLoad()
+    }
+
+    @IBAction private func loginButtonTappped(_ sender: UIButton) {
+        viewModel.loginButtonTapped()
+    }
+}
+
+extension LoginViewController: LoginViewInterface {
+    var userName: String {
+        userNameTextField.text ?? ""
+    }
+    
+    var password: String {
+        passwordTextField.text ?? ""
+    }
+
+    func pushVC() {}
+}
---
+++ FriendsList/FriendsList/Modules/Login/LoginViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewModel.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import Foundation
+
+protocol LoginViewModelInterface {
+    func viewDidLoad()
+    func loginButtonTapped()
+}
+
+final class LoginViewModel {
+    weak var view: LoginViewInterface?
+}
+
+extension LoginViewModel: LoginViewModelInterface {
+    func viewDidLoad() {}
+
+    func loginButtonTapped() {
+        guard let userName = view?.userName, let password = view?.password else {
+            view?.showError(title: "Error", message: "check", buttonTitle: "OK", completion: {})
+        }
+        view.pushVC()
+    }
+}
