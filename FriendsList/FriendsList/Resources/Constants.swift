10399a39b4806cff54585ebf9e10315469b35d1e
ealpago <alpagoemre@icloud.com>
2024-09-30 17:45:08 +0300

    BaseURL added to Constant file

---
+++ FriendsList/FriendsList/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import Foundation
+
+struct APIConstants{
+    static let shared = APIConstants()
+
+    let randomUserBaseURL = "https://randomuser.me/"
+}
