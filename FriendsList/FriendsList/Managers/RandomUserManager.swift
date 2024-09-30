41b5618c40b3ad46e376d17bf97475b9c929e2d7
ealpago <alpagoemre@icloud.com>
2024-09-30 18:28:31 +0300

    RandomUserManager created

---
+++ FriendsList/FriendsList/Managers/RandomUserManager.swift
@@ -0,0 +1,28 @@
+//
+//  RandomUserManager.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import Foundation
+
+class RandomUserManager {
+    static let shared = RandomUserManager()
+    var result: RandomUserResponse?
+
+    func fetchRandomUser(completion: @escaping (Result<RandomUserResponse, NetworkError>) -> ()) {
+        NetworkManager.shared.request(requestRoute: NetworkRouter.users, responseModel: RandomUserResponse.self) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                print("Received repos: \(response)")
+                self.result = response
+                completion(.success(response))
+            case .failure(let error):
+                print("Failed with error: \(error)")
+                completion(.failure(error))
+            }
+        }
+    }
+}
