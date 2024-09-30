168c2d5b031af5337a5127f13c494c608bf6bdef
ealpago <alpagoemre@icloud.com>
2024-09-30 16:28:06 +0300

    First Commit

---
+++ FriendsList/FriendsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendsList
+//
+//  Created by Emre Alpago on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
