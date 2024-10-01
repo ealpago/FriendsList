80992a48a69688d7153875624c5cbd4afa5336ff
ealpago <alpagoemre@icloud.com>
2024-10-01 19:35:44 +0300

    Helpers types now AnyObject

--- FriendsList/FriendsList/Helpers/AlertPresentable.swift
+++ FriendsList/FriendsList/Helpers/AlertPresentable.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-protocol AlertPresentable {
-    func showError(title: String, message: String, buttonTitle: String, completion: @escaping()->())
+protocol AlertPresentable: AnyObject {
+    func showError(title: String, message: String, buttonTitle: String, completion: @escaping () -> ())
 }
 
 extension AlertPresentable where Self: UIViewController {
-    func showError(title: String, message: String, buttonTitle: String, completion: @escaping()->()) {
+    func showError(title: String, message: String, buttonTitle: String, completion: @escaping () -> ()) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: buttonTitle, style: .cancel, handler: { action in
             completion()
