1d06441917a00ae11584195e77c0a5da289a1ad8
ealpago <alpagoemre@icloud.com>
2024-10-01 15:48:23 +0300

    ViewDidLoad test passed

--- FriendsList/FriendsListTests/Modules/Listing/ListingViewModelsTest.swift
+++ FriendsList/FriendsListTests/Modules/Listing/ListingViewModelsTest.swift
@@ -11,26 +11,40 @@ import XCTest
 final class ListingViewModelsTest: XCTestCase {
     private var viewModel: ListingViewModel!
     private var view: MockListingViewController!
+    private var randomUserManager: MockRandomUserManager!
 
     override func setUp() {
         super.setUp()
         view = .init()
-        viewModel = .init(view: view)
+        randomUserManager = .init()
+        viewModel = .init(view: view, randomUserManager: randomUserManager)
     }
 
     override func tearDown() {
         super.tearDown()
     }
-
-    func test_numbberOfRows_userCount() {
+    func test_numbberOfRows_whenAppOnline_userCount() {
         viewModel.isAppOnline = true
 
         XCTAssertEqual(viewModel.isAppOnline, true)
     }
 
-    func test_numbberOfRows_realmUsersCount() {
+    func test_numbberOfRows_whenAppOffline_realmUsersCount() {
         viewModel.isAppOnline = false
 
         XCTAssertEqual(viewModel.isAppOnline, false)
+    }
+
+    func test_viewDidLoad_InvokesRequiredMethods() {
+        //given
+        XCTAssertFalse(view.isBarHidden)
+        XCTAssertFalse(view.isTableViewInvoke)
+
+        //when
+        viewModel.viewDidLoad()
+
+        //then
+        XCTAssertEqual(view.barHiddenCount, 1)
+        XCTAssertEqual(view.invokeTableViewCount, 1)
     }
 }
--- FriendsList/FriendsListTests/Modules/Listing/MockListingViewController.swift
+++ FriendsList/FriendsListTests/Modules/Listing/MockListingViewController.swift
@@ -8,23 +8,29 @@
 @testable import FriendsList
 
 final class MockListingViewController: ListingViewInterface {
-    func hideNavBar() { }
+
+
+    var isBarHidden: Bool = false
+    var barHiddenCount: Int = 0
+    func hideNavBar() {
+        isBarHidden = true
+        barHiddenCount += 1
+    }
     
-    func prepareTableView() {  }
-    
+    var isTableViewInvoke: Bool = false
+    var invokeTableViewCount: Int = 0
+    func prepareTableView() {
+        isTableViewInvoke = true
+        invokeTableViewCount += 1
+    }
+
     func reloadData() { }
     
     func pushVC(argument: FriendsList.DetailViewArguments?) {  }
     
-    func showError(title: String, message: String, buttonTitle: String, completion: @escaping () -> ()) {
-
-    }
+    func showError(title: String, message: String, buttonTitle: String, completion: @escaping () -> ()) {}
     
-    func showLoadingIndicator() {
-
-    }
+    func showLoadingIndicator() {}
     
-    func hideLoadingIndicator() {
-        
-    }
+    func hideLoadingIndicator() {}
 }
