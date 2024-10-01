dfeb2407801de8b1cf987bb736f8c34b87ea7991
ealpago <alpagoemre@icloud.com>
2024-10-01 23:32:53 +0300

    RandomUserManagerInterface cleaned

--- FriendsList/FriendsList/Managers/RandomUserManager.swift
+++ FriendsList/FriendsList/Managers/RandomUserManager.swift
@@ -8,22 +8,16 @@
 import Foundation
 
 protocol RandomUserManagerInterface {
-    var result: RandomUserResponse? { get }
-
     func fetchRandomUser(completion: @escaping (Result<RandomUserResponse, NetworkError>) -> ())
 }
 
 final class RandomUserManager: RandomUserManagerInterface {
-    static let shared = RandomUserManager()
-    var result: RandomUserResponse?
-
     func fetchRandomUser(completion: @escaping (Result<RandomUserResponse, NetworkError>) -> ()) {
         NetworkManager.shared.request(requestRoute: NetworkRouter.users, responseModel: RandomUserResponse.self) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let response):
                 print("Received repos: \(response)")
-                self.result = response
                 completion(.success(response))
             case .failure(let error):
                 print("Failed with error: \(error)")
