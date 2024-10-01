9f3c09d3064f1a423189fa557642f1eb26f0bee9
ealpago <alpagoemre@icloud.com>
2024-10-01 11:54:37 +0300

    ListingViewModelTests file created

---
+++ FriendsList/FriendsListTests/Modules/Listing/ListingViewModelsTest.swift
@@ -0,0 +1,21 @@
+//
+//  ListingViewModelsTest.swift
+//  FriendsListTests
+//
+//  Created by Emre Alpago on 1.10.2024.
+//
+
+import XCTest
+@testable import FriendsList
+
+final class ListingViewModelsTest: XCTestCase {
+    private var viewModel: ListingViewModel!
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
