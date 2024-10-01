fd381f66e63da5940a774e2d923492a7940e856a
ealpago <alpagoemre@icloud.com>
2024-10-01 13:44:10 +0300

    first tests are done

--- FriendsList/FriendsListTests/Modules/Listing/ListingViewModelsTest.swift
+++ FriendsList/FriendsListTests/Modules/Listing/ListingViewModelsTest.swift
@@ -10,12 +10,27 @@ import XCTest
 
 final class ListingViewModelsTest: XCTestCase {
     private var viewModel: ListingViewModel!
+    private var view: MockListingViewController!
 
     override func setUp() {
         super.setUp()
+        view = .init()
+        viewModel = .init(view: view)
     }
 
     override func tearDown() {
         super.tearDown()
+    }
+
+    func test_numbberOfRows_userCount() {
+        viewModel.isAppOnline = true
+
+        XCTAssertEqual(viewModel.isAppOnline, true)
+    }
+
+    func test_numbberOfRows_realmUsersCount() {
+        viewModel.isAppOnline = false
+
+        XCTAssertEqual(viewModel.isAppOnline, false)
     }
 }
